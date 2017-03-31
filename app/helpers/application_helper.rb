ef323fdadacf907916ecac8ed58eda43f7293a80
Rubysdad <jordan@devcamp.com>
2017-03-31 21:26:13 +0000

    Style updates to edit and show user pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,14 @@ module ApplicationHelper
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
-  
+
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} amd you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source_greeting")
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
     end
   end
-  
+
   def copyright_generator
     MulberryViewTool::Renderer.copyright 'Julia Valentine Arizona Properties', 'All rights reserved'
   end
