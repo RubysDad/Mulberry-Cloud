be8d835da1290b81a175b81606cbc039866ac1b4
Rubysdad <jordan@devcamp.com>
2017-04-19 22:59:02 +0000

    fixed realtor-news layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,6 +42,10 @@ module ApplicationHelper
       url: portfolios_path,
       title: 'Portfolio'
     }, 
+    {
+      url: realtor_news_path,
+      title: 'Realtor-news'
+    }, 
   ]
   end
   
