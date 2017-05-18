5c7c66387ca47ec6a62031dced979c0e28e8de7d
Rubysdad <jordan@devcamp.com>
2017-05-18 23:10:57 +0000

    Fixed technologies bug in portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -70,7 +70,7 @@ class PortfoliosController < ApplicationController
                                        :body, 
                                        :main_image,
                                        :thumb_image,
-                                       technologies_attributes: [:id, :name, :_destroy]
+                                       technologies_attributes: [:id, :name, :destroy]
                                        )
   end
   
