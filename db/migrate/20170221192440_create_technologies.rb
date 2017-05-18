ddf27a0eaca246b8af8f592a12c0e1fcaea66131
Rubysdad <jordan@devcamp.com>
2017-05-18 23:27:45 +0000

    fixed bug in tech migration

--- db/migrate/20170221192440_create_technologies.rb
+++ db/migrate/20170221192440_create_technologies.rb
@@ -1,4 +1,4 @@
-class CreateRegions < ActiveRecord::Migration[5.0]
+class CreateTechnologies < ActiveRecord::Migration[5.0]
   def change
     create_table :technologies do |t|
       t.string :name
